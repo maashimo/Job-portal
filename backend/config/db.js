d589c059e6707d6ff7283428c1b77a5d7cbae677
lakshishankari <slshankari16@gmail.com>
2025-08-02 05:44:33 -0700

    update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -3,9 +3,9 @@ const mongoose = require('mongoose');
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/jobwell');
-    console.log(MongoDB Connected: ${conn.connection.host});
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
   } catch (err) {
-    console.error(Error connecting to MongoDB: ${err.message});
+    console.error(`Error connecting to MongoDB: ${err.message}`);
     process.exit(1);
   }
 };
